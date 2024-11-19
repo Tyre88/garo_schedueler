5a4ba0055f8003f650bdb02629a62ad042f0e23c
Victor Öhrström <victor@abrakadabrasoftware.se>
2024-11-19 09:24:07 +0100

    feat: testing to add logging

--- custom_components/garo_schedueler/__init_.py
+++ custom_components/garo_schedueler/__init_.py
@@ -1,7 +1,13 @@
+"""The garo_schedueler component."""
+import logging
+
+_LOGGER = logging.getLogger("custom_components.garo_schedueler.init")
+
 DOMAIN = "garo_schedueler"
 
 
 async def async_setup(hass, config):
+  _LOGGER.info("Setting up garo_schedueler component")
   hass.states.async_set("hello_state.world", "Paulus")
 
   # Return boolean to indicate that initialization was successful.
