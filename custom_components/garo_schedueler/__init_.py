06efdbb610d70bfbc921a05771c2972cb6cd8f6d
Victor Öhrström <victor@abrakadabrasoftware.se>
2024-11-19 08:55:21 +0100

    feat: adding manifest

--- custom_components/garo_schedueler/__init_.py
+++ custom_components/garo_schedueler/__init_.py
@@ -2,7 +2,7 @@ DOMAIN = "garo_schedueler"
 
 
 async def async_setup(hass, config):
-    hass.states.async_set("hello_state.world", "Paulus")
+  hass.states.async_set("hello_state.world", "Paulus")
 
-    # Return boolean to indicate that initialization was successful.
-    return True
+  # Return boolean to indicate that initialization was successful.
+  return True
