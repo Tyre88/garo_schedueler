61dd072b2a287a1cd5cf4a15bac372e4fda7197f
Victor Öhrström <victor@abrakadabrasoftware.se>
2024-11-19 10:51:45 +0100

    fix: testing non async

--- custom_components/garo_schedueler/__init_.py
+++ custom_components/garo_schedueler/__init_.py
@@ -6,9 +6,8 @@ _LOGGER = logging.getLogger("custom_components.garo_schedueler.init")
 DOMAIN = "garo_schedueler"
 
 
-async def async_setup(hass, config):
-  _LOGGER.info("Setting up garo_schedueler component")
-  hass.states.async_set("hello_state.world", "Paulus")
+def setup(hass, config):
+    hass.states.set("garo_schedueler.world", "Paulus")
 
-  # Return boolean to indicate that initialization was successful.
-  return True
+    # Return boolean to indicate that initialization was successful.
+    return True
